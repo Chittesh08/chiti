05976d1d58fac163fc5ed1473bfed8fc6f72a3ac
Chittesh08 <chittesh232005@gmail.com>
2025-01-25 15:19:09 +0530

    Update Even.java

--- Even.java
+++ Even.java
@@ -1,10 +1,11 @@
-public class Even
-{
-public static void main(String args[]) 
-{
-for (int i = 2; i <= 20; i += 2) 
-{
-System.out.print(i + "0 ");
-}
-}
+class Main {
+   public static void main(String args[]) {
+    int num = 100;
+    System.out.print("Odd Numbers from 1 to "+num+" are: ");
+    for (int i = 1; i <= num; i++) {
+       if (i % 2 != 0) {
+        System.out.print(i + " ");
+       }
+    }
+   }
 }
